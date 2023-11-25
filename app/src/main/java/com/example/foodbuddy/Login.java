12d77ebd384976949e9deac27f6aac523ee1dbcd
hodgesboyeli <elijah1.hodges@famu.edu>
2023-11-25 16:58:40 -0500

    Update Login.java

--- app/src/main/java/com/example/foodbuddy/Login.java
+++ app/src/main/java/com/example/foodbuddy/Login.java
@@ -2,7 +2,12 @@ package com.example.foodbuddy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class Login extends AppCompatActivity {
 
@@ -10,5 +15,23 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        final EditText username = findViewById(R.id.tvUsername);
+        final EditText password = findViewById(R.id.tvPassword);
+        Button btLogin = findViewById(R.id.btLogin);
+
+        btLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String enteredUsername = username.getText().toString();
+                String enteredPassword = password.getText().toString();
+
+                if (enteredUsername.equals("john1.doe") && enteredPassword.equals("password")) {
+                    startActivity(new Intent(Login.this, FindFood.class));
+                } else {
+                    Toast.makeText(getBaseContext(), "Incorrect username or password.", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
